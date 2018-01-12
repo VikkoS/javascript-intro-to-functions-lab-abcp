36fcfa59a9f14fe03613fb119d75ab7b74741971
Vika Suhorukova <viktorya.sukhorukova@gmail.com>
2018-01-12 02:49:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,8 +18,10 @@ function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return 'I can\'t hear you!';
   } else {
-    if (string === string.toUpperCase()) {
-      return 
+      if (string === string.toUpperCase()) {
+           return "YES INDEED!";
+      } else {
+            if (string === )
     }
   }
 }
