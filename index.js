bd7b6be550e7c59a84a87f215069f37bb17d6a96
Vika Suhorukova <viktorya.sukhorukova@gmail.com>
2018-01-12 02:32:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function shout(string) {
- return string.toUpperCase()
+ return string.toUpperCase();
 }
 
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
 
 function logShout(string) {
-  string.toUpperCase
-  console.log(string)
+  string.toUpperCase;
+  console.log(string);
 }
